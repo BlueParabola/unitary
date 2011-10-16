4add3860439e744761332002f49fd2d5c00fafb9
Marco Tabini <marcot@tabini.ca>
2011-10-16 10:46:35 -0400

    - Basic app

---
+++ app/src/com/blueparabola/unitary/project/UnitaryProjectPanelClass.as
@@ -0,0 +1,12 @@
+package com.blueparabola.unitary.project
+{
+	import mx.containers.Panel;
+	
+	public class UnitaryProjectPanelClass extends Panel
+	{
+		public function UnitaryProjectPanelClass()
+		{
+			super();
+		}
+	}
+}
---
+++ test_project/test.php
@@ -0,0 +1,17 @@
+<?php
+
+require "script.php";
+
+class addTest extends PHPUnit_Framework_TestCase {
+	public function setUp() {
+		
+	}
+	
+	public function tearDown() {
+		
+	}
+	
+	public function testAddition() {
+		$this->assertTrue(add(1, 2) == 3);
+	}
+}
